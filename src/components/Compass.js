5184b59de92d885fe4fe6237ac7c534f240d5ffd
unknown <ranger@glimpass.com>
2023-09-04 00:31:13 +0530

    Compass circle moving correctly without iphone policy

--- src/components/Compass.js
+++ src/components/Compass.js
@@ -3,29 +3,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Compass.css';
 import { ReactComponent as CompassSVG } from '../assets/compass.svg';
+//import { ReactComponent as Arrow } from '../assets/navigationArrow.svg';
 import Direction from './Direction';
 
 
-
-// function CompassSVG({ rotation }) {
-//     return (
-//       <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-//         {/* ... Rest of the SVG content ... */}
-  
-//         {/* Arrow or pointer. This is what you would rotate */}
-//         <line x1="100" y1="100" x2="100" y2="40" 
-//               stroke="red" stroke-width="4" 
-//               transform={`rotate(${rotation}, 100, 100)`} />
-//         <line x1="100" y1="100" x2="100" y2="160" 
-//               stroke="blue" stroke-width="2" />
-//       </svg>
-//     );
-//   }
-
 function Compass() {
     const [alpha, setAlpha] = useState(0);
-    const needleRef = useRef(null);
+  //  const needleRef = useRef(null);
     const OFFSET = 0;
+    const rotatingPartRef = useRef(null); // Ref for the rotating part of the SVG
+
 
     const [stepCount, setStepCount] = useState(0);
 const accelerometerData = useRef([]);
@@ -90,53 +77,23 @@ useEffect(() => {
     }, []);
 
     useEffect(() => {
-        if (needleRef.current) {
-            needleRef.current.style.transform = `rotate(${-45 + alpha}deg)`;
+        if (rotatingPartRef.current) {
+            rotatingPartRef.current.style.transform = `rotate(-${360 - alpha}deg)`;
         }
-        
 
     }, [alpha]);
 
     return (
-        // <div>
-        //     <div style={{ transform: `rotate(${alpha}deg)` }}>
-        //     <CompassSVG rotation={alpha} />
-        //     </div>
-        //     <p>Alpha: {alpha}</p>
-        // </div>
-
-        // <div className="compass-container">
-        //     {[...Array(360)].map((_, index) => (
-        //         <div 
-        //             key={index}
-        //             style={{
-        //                 position: 'absolute',
-        //                 top: '50%',
-        //                 left: '50%',
-        //                 transform: `rotate(${index}deg) translateY(-50%)`,
-        //                 fontSize: index % 30 === 0 ? '12px' : '0px'
-        //             }}
-        //         >
-        //             {index % 30 === 0 ? index : ''}
-        //         </div>
-        //     ))}
-        //     <div 
-        //         className="compass-arrow" 
-        //         style={{ transform: `translateX(-50%) rotate(${alpha}deg)` }}
-        //     ></div>
-        // </div>
+       
 <>
         <div className="compass-container">
-            <CompassSVG className="compass-image" ref={needleRef} />
-            <div className="compass-label north">N</div>
-            <div className="compass-label south">S</div>
-            <div className="compass-label west">W</div>
-            <div className="compass-label east">E</div>
-            
-        alpha : {parseInt(alpha)}
+            <p>↓</p>
+            <CompassSVG className="compass-image" ref={rotatingPartRef} />
+        alpha : {360 - parseInt(alpha)}
         </div>
+        <br></br><br></br>
         <div className="step-counter">steps: {stepCount}</div>
-
+<br></br>
         <Direction/>
         </>
     );
